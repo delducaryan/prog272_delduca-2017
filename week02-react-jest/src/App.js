adfbe9e61dd6efa4bc34e0cb24c7f2f00dad3e0a
Ryan at school <ryanofthedukes@gmail.com>
2017-04-12 17:15:10 -0700

    week02 code update

--- week02-react-jest/src/App.js
+++ week02-react-jest/src/App.js
@@ -1,21 +1,39 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends Component
+{
+    constructor()
+    {
+        super();
+        this.state =
+        {
+            nine: '0'
+        }
+    }
+
+    getNine = () =>
+    {
+        this.setState({ nine: "9"});
+    };
+
+    render()
+    {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    Nine: { this.state.nine }
+                </p>
+
+                <button onClick={this.getNine}>Get Nine</button>
+            </div>
+        );
+    }
 }
 
 export default App;
--- week02-react-jest/src/App.test.js
+++ week02-react-jest/src/App.test.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ReactBasics } from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('React Jest Suite', function ()
+{
+    it('renders without our App component without crashing', () =>
+    {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+    });
+
+    it('renders and reads H1 text', () =>
+    {
+        const wrapper = shallow(<App />);
+        const welcome = <h2>Welcome to React</h2>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders and displays the word Nine', () =>
+    {
+        const wrapper = shallow(<App />);
+        const welcome = <p>'Nine: 0'</p>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders button click message', () =>
+    {
+        const wrapper = shallow(<App />);
+        const nineSign = <p>Nine: 9</p>;
+        wrapper.find('button').simulate('click');
+        expect(wrapper.contains(nineSign)).toEqual(true);
+    });
 });
