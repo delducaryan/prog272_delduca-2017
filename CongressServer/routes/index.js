594034d1e7109d2d5e3fc36a4f731da3d6bfd938
bcuser <ryanofthedukes@gmail.com>
2017-06-08 16:52:04 -0700

    Mongoose

--- CongressServer/routes/index.js
+++ CongressServer/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var allMongo = require('./all-mongo');
+var connect = require('./connect');
+//var politicians = require('../models/politicians');
 
 /* GET home page. */
-router.get('/', function(req, res, next) { 'use strict';
-  res.render('index', { title: 'CongressServer' });
+router.get('/', function(req, res, next)
+{
+    'use strict';
+    res.render('index', { title: 'CongressServer' });
+});
+
+router.get('/bar', function(request, response)
+{
+    response.status(200).send({result: 'bar'});
+});
+
+router.get('/:id', function(request, response)
+{
+    'use strict';
+    response.status(200).send({ result: request.params.id });
 });
 
 module.exports = router;
